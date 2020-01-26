ce241a940f7844dc280abd2295d62bd0565c88a9
James <58656026+nomadjsdev@users.noreply.github.com>
2020-01-26 23:01:46 +0700

    Patch issue with importing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-export {
-  ButtonGroup,
-  MessageButton,
-  TimerButton,
-  ToggleButton
-} from 'Components'
+export { default as ButtonGroup } from './Components/ButtonGroup'
+export { default as MessageButton } from './Components/MessageButton'
+export { default as TimerButton } from './Components/TimerButton'
+export { default as ToggleButton } from './Components/ToggleButton'
